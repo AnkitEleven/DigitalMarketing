c0592bd1ff4887deab9bf3e66b0f6ef9d88beae0
AnkitEleven <userankit1839@gmail.com>
2023-12-24 11:46:13 +0530

    second commit

---
+++ js/all-linke.js
@@ -0,0 +1,8 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="js/core.min.js"></script>
+<script src="js/search.js"></script>
+<script src="js/main.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/scripts.js"></script>
